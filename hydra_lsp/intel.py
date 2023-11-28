2b7327fc7f4b33d73376671012a0bc0bbcd4211c
Andrii Zhuravchak <retsediv1@gmail.com>
2023-11-28 13:00:47 +0100

    :hammer: Refactor Intel class

--- hydra_lsp/intel.py
+++ hydra_lsp/intel.py
@@ -2,26 +2,59 @@ from __future__ import annotations
 
 import json
 import logging
+from functools import wraps
+from typing import Tuple
 
 from lsprotocol import types as lsp_types
 from pygls.server import LanguageServer
+from pygls.workspace import TextDocument
 
 from hydra_lsp.parser import HydraContext
-from hydra_lsp.utils import to_markdown_content, yaml_get_key, yaml_get_variable_name
+from hydra_lsp.utils import (
+    to_markdown_content,
+    yaml_get_identifier,
+    yaml_get_variable_name,
+)
 
 logger = logging.getLogger(__name__)
 
 
-# TODO: this class has a lot of duplicated code
+def intel(feature: str, context_required: bool = True):
+    def wrapper(method):
+        @wraps(method)
+        def _impl(self, *args, **kwargs):
+            logger.info(f"{feature} requested: {args[0]}")
+
+            if context_required and args[1] is None:
+                logger.warning("Context is not loaded")
+                return None
+
+            return method(self, *args, **kwargs)
+
+        return _impl
+
+    return wrapper
+
+
 class HydraIntel:
     def __init__(self, ls: LanguageServer) -> None:
         self.ls = ls
 
-    def get_hover(
+    def _get_location(
         self,
-        params: lsp_types.HoverParams,
-        context: HydraContext | None,
-        debug_hover: bool = False,
+        params: lsp_types.TextDocumentPositionParams
+        | lsp_types.HoverParams
+        | lsp_types.ReferenceParams,
+    ) -> Tuple[str, TextDocument, lsp_types.Position]:
+        document_path = params.text_document.uri
+        document = self.ls.workspace.get_document(document_path)
+        position = params.position
+
+        return document_path, document, position
+
+    @intel("Hover")
+    def get_hover(
+        self, params: lsp_types.HoverParams, context: HydraContext | None
     ) -> lsp_types.Hover | None:
         """
         Get hover information:
@@ -32,17 +65,7 @@ class HydraIntel:
             it will return the computed value of the key.
         """
 
-        document_path = params.text_document.uri
-        document = self.ls.workspace.get_document(document_path)
-        position = params.position
-
-        if debug_hover:
-            logger.info(f"Hover requested: {document_path} at {position}")
-
-        if context is None:
-            logger.warning("Context is not loaded")
-            return None
-
+        document_path, document, position = self._get_location(params)
         current_line = document.lines[position.line]
 
         # check if the cursor is before or after the ':'
@@ -62,55 +85,31 @@ class HydraIntel:
         s = json.dumps({key: value}, indent=2)[1:-1]
         return lsp_types.Hover(contents=to_markdown_content(s))
 
-    # TODO: get_definition and get_references are very similar, we should merge them somehow
+    @intel("Definition")
     def get_definition(
         self,
         params: lsp_types.TextDocumentPositionParams,
         context: HydraContext | None,
     ):
         """Get definition of the variable."""
-
-        logger.info(
-            f"Definition requested: {params.text_document.uri} at {params.position}"
-        )
-        document_path = params.text_document.uri
-        document = self.ls.workspace.get_document(document_path)
-        position = params.position
-
+        document_path, document, position = self._get_location(params)
         current_line = document.lines[position.line]
+
         key = yaml_get_variable_name(current_line, position.character)
-
-        if context is None:
-            logger.warning("Context is not loaded")
-            return None
-
         if key is None:
             return None
 
         logger.info(f"Definition of {key} is {context.definitions.get(key)}")
         return context.definitions.get(key)
 
+    @intel("References")
     def get_references(
         self, params: lsp_types.ReferenceParams, context: HydraContext | None
     ):
         """Get references of the variable."""
-
-        logger.info(
-            f"References requested: {params.text_document.uri} at {params.position}"
-        )
-        document_path = params.text_document.uri
-        document = self.ls.workspace.get_document(document_path)
-        position = params.position
-
+        document_path, document, position = self._get_location(params)
         current_line = document.lines[position.line]
-        key = yaml_get_variable_name(current_line, position.character)
-
-        if context is None:
-            logger.warning("Context is not loaded")
-            return None
-
-        if key is None:
-            key = yaml_get_key(current_line, position.character)
+        key = yaml_get_identifier(current_line, position.character)
 
         if key is None:
             return None
@@ -121,6 +120,7 @@ class HydraIntel:
     def get_diagnostics(self, context: HydraContext | None, doc_uri: str | None):
         """Get diagnostics for the current context."""
 
+        logger.info(f"Diagnostics requested for: {doc_uri}")
         if context is None:
             logger.warning("Context is not loaded")
             return None
